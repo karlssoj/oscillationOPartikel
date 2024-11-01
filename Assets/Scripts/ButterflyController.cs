35ace9eee69e4f3e6226e48ea5fd3e7c693b7a31
karlssoj <karlssoj@arcada.fi>
2024-11-01 12:00:33 +0200

    Added wing rotation

--- Assets/Scripts/ButterflyController.cs
+++ Assets/Scripts/ButterflyController.cs
@@ -1,4 +1,6 @@
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class ButterflyController : MonoBehaviour
 {
@@ -6,13 +8,20 @@ public class ButterflyController : MonoBehaviour
     public float FlyFrequency;
 
     
-    public float FlyFrequencey;
+    public float WingAmplitude;
     public float WingFrequency;
 
 
     Vector3 Position;
+    Vector3 Rotation;
 
     GameObject RearWingL;
+    GameObject RearWingR;
+    GameObject FrontWingL;
+    GameObject FrontWingR;
+    
+
+    float InitalPosition;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,22 +29,36 @@ public class ButterflyController : MonoBehaviour
     {
         Position = transform.position;
         RearWingL = GameObject.Find("RearWingL");
+        RearWingR = GameObject.Find("RearWingR");
+        FrontWingL = GameObject.Find("FrontWingL");
+        FrontWingR = GameObject.Find("FrontWingR");
+        Rotation = RearWingL.transform.rotation.eulerAngles;
+        InitalPosition = transform.position.y;
+
     }
 
     // Update is called once per frame
     void Update()
     {
          FlyMotion();
+         WingMotion();
+
+         transform.Translate(0, 0, 1 * Time.deltaTime);
     }
 
     void WingMotion()
     {
-        //RearWingL.transform.rotation.eulerAngles.z;
+        Rotation.z = WingAmplitude * Mathf.Sin(2*Mathf.PI*WingFrequency*Time.time);
+
+        RearWingL.transform.eulerAngles = -Rotation;
+        RearWingR.transform.eulerAngles = Rotation;
+        FrontWingL.transform.eulerAngles = -Rotation;
+        FrontWingR.transform.eulerAngles = Rotation;
     }
 
     void FlyMotion()
     {
-        Position.y = FlyAmplitude * Mathf.Sin(2*Mathf.PI*FlyFrequencey*Time.time);
+        Position.y = FlyAmplitude * Mathf.Sin(2*Mathf.PI*FlyFrequency*Time.time) + InitalPosition;
 
         transform.position = Position;
     }
