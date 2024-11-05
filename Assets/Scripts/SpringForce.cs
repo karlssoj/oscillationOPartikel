485908044c6232ea2e1f6f5744ffafd84a05079a
karlssoj <karlssoj@arcada.fi>
2024-11-05 02:26:59 +0200

    Exemplet klart

--- Assets/Scripts/SpringForce.cs
+++ Assets/Scripts/SpringForce.cs
@@ -4,22 +4,41 @@ public class SpringForce : MonoBehaviour
 {
     public float Mass;
     public float K;
+    public float Damping;
     float G = -9.82f;
+    float Y0;
 
     Vector3 Velocity;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Velocity = new Vector3(0, 0, 0);
+        Y0 = transform.position.y;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        //Beräknar tyngkraften
         float Fg = Mass * G;
-        float acceleration = Fg/Mass * Time.deltaTime;
 
+        //Beräknar fjäderkraften
+        float DeltaY = Y0 - transform.position.y;
+        float Ff = DeltaY * K;
+
+        //BEräknar den resulterande kraften
+        float Fr = Ff + Fg;
+
+        //Omvandlar resulterande kraften i acceleration
+        float acceleration = Fr/Mass * Time.deltaTime;
+
+        //Adderar accelerationen till hastigheten
         Velocity.y += acceleration;
+
+        //Friktion
+        Velocity *= Damping;
+        
+        //Adderar hastigheten till positionen
         transform.Translate(Velocity * Time.deltaTime);
     }
 }
